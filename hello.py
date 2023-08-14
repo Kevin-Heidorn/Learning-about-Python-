a764736029e0373fd93a13489b2dd17670a46670
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2023-08-13 21:22:23 -0300

    First day programming in python

---
+++ hello.py
@@ -0,0 +1,7 @@
+#Ask user for their name
+name = input("What 's your name ?")
+#Say Hello to user if your name 
+#print("Hello, " , name) "Name " is a String 
+#print("Hello, " + name) "Name " is a String 
+#print ("Hello, ",name, end="!") "When you use this you can put something "
+print("Hello ,", name , sep=" 1 ")
