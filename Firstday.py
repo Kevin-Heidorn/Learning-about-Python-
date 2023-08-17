d1a21e00ef2db39eba92fe63e5daa9e9c39d7399
Kevin Heidorn <128178416+Kevin-Heidorn@users.noreply.github.com>
2023-08-17 20:56:26 -0300

    Update Firstday.py

--- Firstday.py
+++ Firstday.py
@@ -1,7 +1,6 @@
-#Ask user for their name
-name = input("What 's your name ?")
-#Say Hello to user if your name 
-#print("Hello, " , name) "Name " is a String 
-#print("Hello, " + name) "Name " is a String 
-#print ("Hello, ",name, end="!") "When you use this you can put something "
-print("Hello ,", name , sep=" 1 ")
+class ClownFish(): 
+pass
+
+fish = ClownFish() 
+
+isinstance(fish, ClownFish) 
